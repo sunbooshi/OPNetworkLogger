30093e220e5da021d2e98c9284a3f14156bfbc69
sunboshi <boshi@sunboshi.tech>
2018-09-05 15:53:21 +0800

    Add log parser src.

---
+++ OPNetworkLogger/logparser/src/App.vue
@@ -0,0 +1,166 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="main">
+        <div class="header">
+          <input v-model="search" class="input-search" placeholder="🔍请输入搜索内容"/>
+          <button @click="onSearch">搜索</button>
+          <button @click="onRest">重置</button>
+          <button @click="loadLog">刷新</button>
+        </div>
+        <div class="log-container">
+          <v-table
+            class="log-table"
+            :width="700"
+            :height="600"
+            :columns="columns"
+            :column-cell-class-name="columnCellClass"
+            :table-data="tableData"
+            :show-vertical-border="false"
+            row-hover-color="#eee"
+            row-click-color="#edf7ff"
+            :row-click="onClick"
+            :row-height="25"
+          ></v-table>
+          </div>
+      </div>
+      <div class="right-panel">
+        <Detail :request="selectedRequest"></Detail>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Detail from './Detail.vue'
+
+export default {
+  name: 'app',
+  components: {Detail},
+  data () {
+    return {
+      search: '',
+      logs: [],
+      tableData: [],
+      selectedRequest: {},
+      columns: [
+        {field: 'url', title:'Domain', titleAlign: 'left',columnAlign:'left'},
+        {field: 'method', title: 'Method', width: 60, titleAlign: 'left',columnAlign:'left'},
+        {field: 'status', title: 'Status', width: 60, titleAlign: 'left',columnAlign:'left'},
+        {field: 'duration', title: 'Duration', width:60, titleAlign: 'left',columnAlign:'left'},
+        {field: '', title: '', width:20, titleAlign: 'left', columnAlign:'right'},
+      ]
+    }
+  },
+  created() {
+    this.loadLog();
+  },
+  methods: {
+    loadLog () {
+      fetch('http://localhost:10086/log')
+        .then(resp => resp.json())
+        .then(resp => {
+          this.logs = resp;
+          this.tableData = resp.slice(0);
+        })
+    },
+    onSearch() {
+      if (this.search.length > 0) {
+        var results = new Array();
+        for (var i in this.logs) {
+          var log = this.logs[i];
+          console.log(log.url);
+          if (log.url.indexOf(this.search) > 0) {
+            results.push(log);
+          }
+        }
+        this.tableData = results;
+      }
+    },
+    onRest() {
+      this.search = '';
+      this.tableData = this.logs.slice(0);
+    },
+    onClick(rowIndex, rowData, column) {
+//      console.log(rowData);
+      this.selectedRequest = rowData;
+    },
+    columnCellClass(rowIndex,columnName,rowData) {
+      if (columnName === 'duration') {
+        return 'cell-duration';
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    border: 1px solid #999999;
+    width: 1024px;
+    height: 640px;
+    margin-top: 20px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .container {
+    display: flex;
+    flex-direction: row;
+  }
+  .right-panel {
+    width: 323px;
+    height: 640px;
+  }
+  .main {
+    height: 640px;
+    width: 701px;
+    display: flex;
+    flex-direction: column;
+    border-right: 1px solid #999999;
+  }
+  .header {
+    background-color: #eeeeee;
+    width: 100%;
+    height: 38px;
+    display: flex;
+    flex-direction: row;
+  }
+  .log-container {
+    width: 100%;
+    height: 600px;
+    overflow:auto;
+    text-align: center;
+  }
+  .log-table {
+    width: 100%;
+    height: 100%;
+  }
+  .log-detail {
+    width: 100%;
+    height: 100%;
+  }
+  .v-table-class	 {
+    font-size: 13px;
+  }
+  .cell-duration{
+    margin-right: 40px;
+    padding-right: 40px;
+  }
+  .input-search {
+    margin: 5px;
+    width: 500px;
+    padding-left: 5px;
+  }
+  button {
+    margin: 5px;
+    border-radius: 4px;
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+</style>
---
+++ OPNetworkLogger/logparser/src/Detail.vue
@@ -0,0 +1,136 @@
+<template>
+  <div id="detail">
+    <div class="label"> Full URL </div>
+    <div class="content">
+      {{ request.url }}
+    </div>
+
+    <div class="label"> cURL </div>
+    <div class="content"> {{ curlCommand }} </div>
+
+    <div class="label"> Request Header </div>
+    <div class="headers">
+      <ul v-for="header in request.requestHeaders">
+        <li> <strong>{{ header.key }} :</strong> {{ header.value }} </li>
+      </ul>
+    </div>
+
+    <div class="label"> Request Data </div>
+    <div class="content"> {{ requestData }} </div>
+
+    <div class="label"> Response Header </div>
+    <div class="headers">
+      <ul v-for="header in request.responseHeaders">
+        <li> <strong>{{ header.key }} :</strong> {{ header.value }} </li>
+      </ul>
+    </div>
+
+    <div class="label"> Response Data </div>
+    <div class="content"> {{ responseData }} </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Detail',
+    props : {
+      request: {
+        type: Object,
+        required: false
+      }
+    },
+    computed: {
+      requestData: function () {
+        if (this.request.requestData == null) {
+          return '';
+        }
+        else {
+          return window.atob(this.request.requestData);
+        }
+      },
+      responseData: function () {
+        if (this.request.responseData == null) {
+          return '';
+        }
+        else {
+          return window.atob(this.request.responseData);
+        }
+      },
+      curlCommand: function () {
+        if (typeof(this.request.url) == "undefined") {
+          return ''
+        }
+        var cmd = 'curl ' + '"' + this.request.url + '"' + ' ';
+        for (var i in this.request.requestHeaders) {
+          var header = this.request.requestHeaders[i];
+          cmd = cmd + '-H ' + '"' + header.key + ':' + header.value + '" '
+        }
+        if (this.requestData.length > 0) {
+            cmd = cmd + '-d "' + this.requestData + '"';
+        }
+        return cmd;
+      }
+    },
+    methods: {
+      parseUrl (url) {
+        var param = new Array()
+        var items;
+        if (url == "") {
+          return param;
+        };
+        var query = url
+        var list = query.split('&');
+        for(var i = 0; i < list.length; i++) {
+          items = list[i].split('=');
+          param.push({'key' : items[0], 'value' : items[1]});
+        };
+        return param;
+      }
+    }
+  };
+</script>
+
+<style>
+
+  #detail {
+    overflow: auto;
+    height: 100%;
+    padding-right: 3px;
+    word-break:break-all;
+    width: 324px;
+  }
+
+  .label {
+    padding: 4px;
+    padding-left: 10px;
+    font-size: 14px;
+    background-color: #eeeeee;
+    border-bottom: 1px solid #999999;
+  }
+
+  .content {
+    padding: 0 10px;
+    font-size: 13px;
+  }
+
+  .headers {
+
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+
+  li {
+    font-size: 13px;
+    display: inline-block;
+    margin: 0;
+    padding: 0;
+  }
+
+</style>
